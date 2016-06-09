ac03a27df92cf15e51ccb438b0e9a276c2d6a89c
rlopez <rlopez@debian.deb>
2016-06-09 15:22:24 -0600

    create client

--- Supermercado.c
+++ Supermercado.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "Supermercado.h"
 
 int getNumCarretas(){
   int number;
@@ -7,16 +8,7 @@ int getNumCarretas(){
   scanf ("%d", &number);
   printf ("You entered: %d \n", number);
   return number;
-
 }
-
-typedef struct Carreta
-{
-  int *noCarreta;
-  int size;
-  int capacity;
-    
-}Carreta;
 
 Carreta * createStackCarr(int maxElements)
 {
@@ -102,16 +94,118 @@ void createStacksForCarretas(){
   }
 }
 
-int main() {
-  createStacksForCarretas();
-  return 0;
+struct Node* head; // global variable - pointer to head node.
+int clientId = 0;
+
+//Creates a new Node and returns pointer to it. 
+struct Node* getNewNode(int gen, int ag, int preg, int id){
+	struct Node* newNode
+		= (struct Node*)malloc(sizeof(struct Node));
+	newNode->gender = gen;
+	newNode->age = ag;
+	newNode->pregnant = preg;
+	newNode->id = clientId;
+	newNode->prev = NULL;
+	newNode->next = NULL;
+	return newNode;
+}
+
+//Inserts a Node at head of doubly linked list
+void insertAtHead(int gen, int ag, int preg, int id) {
+  struct Node* newNode = getNewNode(gen, ag , preg, id);
+	if(head == NULL) {
+		head = newNode;
+		return;
+	}
+	head->prev = newNode;
+	newNode->next = head; 
+	head = newNode;
+}
+/*
+//Inserts a Node at tail of Doubly linked list
+void InsertAtTail(int x) {
+	struct Node* temp = head;
+	struct Node* newNode = GetNewNode(x);
+	if(head == NULL) {
+		head = newNode;
+		return;
+	}
+	while(temp->next != NULL) temp = temp->next; // Go To last Node
+	temp->next = newNode;
+	newNode->prev = temp;
+}
+*/
+//Prints all the elements in linked list in forward traversal order
+void print() {
+	struct Node* temp = head;
+	printf("Forward: ");
+	while(temp != NULL) {
+	  printf("********");
+		printf("id %d \n",temp->id);
+		printf("edad%d \n", temp->age);
+		temp = temp->next;
+	}
+	printf("\n");
+}
+/*
+//Prints all elements in linked list in reverse traversal order. 
+void ReversePrint() {
+	struct Node* temp = head;
+	if(temp == NULL) return; // empty list, exit
+	// Going to last Node
+	while(temp->next != NULL) {
+		temp = temp->next;
+	}
+	// Traversing backward using prev pointer
+	printf("Reverse: ");
+	while(temp != NULL) {
+		printf("%d ",temp->data);
+		temp = temp->prev;
+	}
+	printf("\n");
+}
+*/
+void createClient(){
+  int gender = getGender();
+  int pregnant = setPregnant();
+  if(gender = 0){
+    pregnant = 3;
+  }
+  insertAtHead(gender, getAge(), pregnant, clientId++);
+}
+
+int getGender(){
+  return rand() % 1;
+}
+
+int getAge(){
+  return rand() % 100;
+}
+
+int setPregnant(){
+  return rand() % 1;
+}
+
+int getClientNumber(){
+  int number;
+  printf ("Ingresa el numero de Clientes");
+  scanf ("%d", &number);
+  printf ("You entered: %d \n", number);
+  return number;
+}
+
+int setClientsOnList(){
+  int i = 0;
+  int clientNumber = getClientNumber();
+  for(i ; i < clientNumber ; i++){
+    createClient();
+  }
 }
 
 
-
-
-
-
-
-
-
+int main() {
+  createStacksForCarretas();
+  setClientsOnList();
+  print();
+  return 0;
+}
---
+++ Supermercado.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Carreta
+{
+  int *noCarreta;
+  int size;
+  int capacity;
+    
+}Carreta;
+
+
+typedef struct Node  {
+  int gender;
+  int age;
+  int pregnant;
+  int id;
+  struct Node* next;
+  struct Node* prev;
+}Node;
+
+
+
+
